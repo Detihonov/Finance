c7a528fdbbc21235ff297df11c6402a3981e51f0
Denis_Tihonov <ndenmru@gmail.com>
2025-06-01 10:43:49 +0300

    Create Class UserSettings.cs

--- Finance/Data/UserSettings.cs
+++ Finance/Data/UserSettings.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace Finance.Data
 {
-    public class SettingsRepository
+    public class UserSettings
     {
-        private readonly string _filePath = @"Save\settings.json";
+
     }
 }
