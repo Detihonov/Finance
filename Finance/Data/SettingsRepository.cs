f2f2c15eb4c9ab029260343891583bbd700a656e
Denis_Tihonov <ndenmru@gmail.com>
2025-06-01 11:09:26 +0300

    Update Settings add method Load

--- Finance/Data/SettingsRepository.cs
+++ Finance/Data/SettingsRepository.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Finance.Data
@@ -9,5 +11,17 @@ namespace Finance.Data
     public class SettingsRepository
     {
         private readonly string _filePath = @"Save\settings.json";
+
+        public UserSettings Load()
+        {
+            if (!File.Exists(_filePath))
+            {
+                 return new UserSettings();
+            }
+
+            var json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<UserSettings>(json);
+        }
+
     }
 }
