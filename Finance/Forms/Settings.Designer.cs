365ec62e0d71adcd13bde5e2503690078cb6c2de
Denis_Tihonov <ndenmru@gmail.com>
2025-06-03 13:51:40 +0300

    Update Settings form add Button Save

--- Finance/Forms/Settings.Designer.cs
+++ Finance/Forms/Settings.Designer.cs
@@ -30,15 +30,22 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.coboxUser = new System.Windows.Forms.ComboBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textName);
             this.panel1.Controls.Add(this.coboxUser);
             this.panel1.Location = new System.Drawing.Point(6, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(269, 100);
+            this.panel1.Size = new System.Drawing.Size(269, 71);
             this.panel1.TabIndex = 0;
             // 
             // coboxUser
@@ -49,15 +56,53 @@
             this.coboxUser.Size = new System.Drawing.Size(121, 21);
             this.coboxUser.TabIndex = 0;
             // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(6, 36);
+            this.textName.Multiline = true;
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(121, 21);
+            this.textName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Имя";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Location = new System.Drawing.Point(8, 82);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(266, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(3, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Сохранение";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
             // Settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(281, 450);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Settings";
             this.Text = "Settings";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -66,5 +111,9 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ComboBox coboxUser;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSave;
     }
 }
--- Finance/Forms/Settings.cs
+++ Finance/Forms/Settings.cs
@@ -20,5 +20,20 @@ namespace Finance.Forms
             InitializeComponent();
             coboxUser.Items.AddRange(new[] { "₽", "$", "€", "¥", "₤" });
         }
+
+        private void LoadSettings()
+        {
+            userSettings = settingsRepository.Load();
+            textName.Text = userSettings.UserName;
+            coboxUser.SelectedItem = userSettings.CurrentSymbol;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            userSettings.UserName = textName.Text;
+            userSettings.CurrentSymbol = coboxUser.SelectedItem.ToString();
+            settingsRepository.Save(userSettings);
+            MessageBox.Show("Настройки сохранены","Успех",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
     }
 }
